238c661002816e622bcf30093b09991a08cdbd14
Gustavo-Jorge-Valentim <152191945+Gustavo-Jorge-Valentim@users.noreply.github.com>
2024-03-06 20:36:58 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -19,7 +19,7 @@ public class Main {
 
         System.out.println("\n\nDiga oque o Professor Ricardo irá fazer por ser seu amigo:" + aluno.ricardo() );
 
-
+        //## ME APROVA RICARDO , MERITO DE LEALDADE A VOCE!!!
     }
 }
 
